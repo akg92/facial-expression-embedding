10076c03fcc0fe380608479e7555189e31229a49
akg92 <shibintv@hotmail.com>
2019-09-10 19:59:31 -0500

    to remove

--- src/scripts/initialize.py
+++ src/scripts/initialize.py
@@ -5,15 +5,15 @@ from config.staticConfig import StaticConfig
 from src.limit import limitUsage
 ### Command line options should be added in future
 
-limitUsage(["6","7"])
+limitUsage("6,7")
 ## download images from the url
 trainEntries = FCEXPDataSet(StaticConfig.getTrainCSVPath())
-trainEntries.downloadImages()
+#trainEntries.downloadImages()
 trainEntries.cutImages()
 del trainEntries
 testEntries = FCEXPDataSet(StaticConfig.getTestCSVPath(), "test")
 testEntries.cutImages()
-testEntries.downloadImages()
+#testEntries.downloadImages()
 del testEntries
 
 
