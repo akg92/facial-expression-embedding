bd0615b9e5377194acb2a7db4bd7bb40b90331eb
shibin <shibintv@hotmail.com>
2019-10-01 23:17:51 -0500

    remove the validation

--- src/experiment/load_facenet_script_modified.py
+++ src/experiment/load_facenet_script_modified.py
@@ -208,13 +208,12 @@ train_x, train_y, val_x, val_y = get_train_data('../../data/processed_faceexp-co
 
 from keras.callbacks import ModelCheckpoint
 batch_size = 64
-filepath="weights-improvement-{epoch:02d}-{val_accuracy_c:.2f}.hdf5"
+filepath="./dropout2/weights-improvement-{epoch:02d}-{val_accuracy_c:.2f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, save_best_only=False)
 callbacks_list = [checkpoint]
 samples_per_epoch= 500
-history = embedding_model.fit_generator(data_generator_threaded(train_x, train_y, batch_size), 
-    validation_data = validation_generator(val_x, val_y,batch_size), 
-    validation_steps = 1 , samples_per_epoch= samples_per_epoch,callbacks=callbacks_list, 
+history = embedding_model.fit_generator(data_generator_threaded(train_x, train_y, batch_size),
+    samples_per_epoch= samples_per_epoch,callbacks=callbacks_list, 
     nb_epoch=50, workers= 5, use_multiprocessing= True, max_queue_size=25 )
 
 
